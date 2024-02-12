bfdbae3126e5195961fcb07e8b66b2bec3deabec
Will Kumagawa <williamkumagawa@gmail.com>
2024-02-11 17:13:26 -0700

    preparing to eject to fix file sturcture

--- src/api/test.js
+++ src/api/test.js
@@ -20,7 +20,7 @@ const Login = {
 const token = Promise.resolve(Login.login())
 
 const Reddit = {
-    popular() {;
+    popular() {
         var myHeaders = new Headers();
         myHeaders.append('Authorization', `Bearer ${token}`);
         myHeaders.append("Cookie", "csv=2; edgebucket=0o6R2BILYSco0yWoOx; loid=000000000tqabpxwo3.2.1707346576990.Z0FBQUFBQmx4QXE0YjVNMnFRUU5qVVBIMFhacXhEWjlTdkx6UjdvdFcxQmJ0aWJqWkFBek0xdjN3UFduNmhobjBCOU1ZMEJlVUYtcW1nMzVhaFp6cUZvX3AwQ0JXeG5aTm9hcFdUMDdQaEg0V3pFelNNYndLYy1KT0p1b1plQmVneU9NUHRGYkJhS1c; session_tracker=cmccpeklieqmbdaghn.0.1707346616219.Z0FBQUFBQmx4QXE0blphVzBPN2NoUWpjSDhtbWlwUnA1bG8tNHdtMms0WDdDelROak5WM2NUSEdkWjNZck55RzBxbnNUNms2eUt6MHgyYnotUkdrSmlkSktJM0Voc0o2MmxlZklUR3pMenlmVDRVWlFobmlHQlF3T0lMYkczZnNYNzg0WDZYV2VaRVg");
